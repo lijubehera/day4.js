778fee08febfe41bcae9846bd8898b002d5af871
Narayan Behera <146068913+lijubehera@users.noreply.github.com>
2025-06-20 20:45:07 +0530

    Add files via upload

---
+++ day4.js
@@ -0,0 +1,11 @@
+// This is a single-line comment
+let name = "Narayan";  // Inline comment
+
+/*
+This is a multi-line comment.
+It can span across multiple lines.
+Great for explaining code blocks.
+*/
+let age = 25;
+
+// console.log("This won't run");
